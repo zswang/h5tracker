da48743771c3f9e46667554004ef700082742159
zswang <wjhu111@21cn.com>
2016-05-14 18:40:24 +0800

    优化加载代码

--- src/inline.js
+++ src/inline.js
@@ -1,4 +1,4 @@
-(function(window, document, name) {
+(function(window, document, scriptText, name) {
 
   if (!document.querySelector) { // 非 HTML5 环境
     return;
@@ -7,8 +7,8 @@
 
   window[name] = window[name] || function(cmd, src) {
     if (cmd === 'cdn') {
-      var lastScript = document.querySelector('script');
-      var script = document.createElement('script');
+      var lastScript = document.querySelector(scriptText);
+      var script = document.createElement(scriptText);
       script.async = true;
       script.src = src;
       lastScript.parentNode.insertBefore(script, lastScript);
@@ -17,12 +17,16 @@
     }
   };
 
- try{
-    var config = JSON.parse(document.querySelector('[h5t-config]').getAttribute('h5t-config'));
-    window[name]('cdn', config.cdn);
-  }catch(ex){}
+  var script = document.querySelector(scriptText + '[h5t-config]');
+  if (script) {
+    try {
+      var config = JSON.parse(script.getAttribute('h5t-config'));
+      window[name]('cdn', config.cdn);
+    } catch (ex) {
+      console.error(ex.message);
+    }
+  }
 
   window[name].l = window[name].l || Date.now();
 
-})(window, document, 'h5t');
-
+})(window, document, 'script', 'h5t');
